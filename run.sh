4b15bea91949b7cd7ae7937b54d043c0c933921e
Chibuzor Obiora <218098562@student.uj.ac.za>
2022-10-10 18:58:52 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+
+#!/bin/sh
+
+echo "Create an environment...."
+python3 -m venv eflask
+echo "Done!"
+
+echo "Activating environment....."
+. eflask/bin/activate
+echo "Done!"
+
+echo "Installing all required dependencies....."
+pip install flask
+
+pip install pandas
+
+pip install numpy
+
+pip install sklearn
+
+pip install gunicorn
+
+pip install flask-cors
+
+pip install keras
+
+pip install tensorflow-cpu
+
+echo "Done!"
+
+echo "Starting server..."
+gunicorn wsgi:app
